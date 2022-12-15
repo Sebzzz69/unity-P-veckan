e30dd8183a086f96ee1e3743ca225a894507a571
Sebzzz69 <callenholmljungsebastian@gmail.com>
2022-12-15 23:47:55 +0100

    Credits menu - full game done

--- Blobfisk/Assets/Scripts/MainMenu.cs
+++ Blobfisk/Assets/Scripts/MainMenu.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     MusicScript musicScript;
+    [SerializeField] GameObject menuUI;
+    [SerializeField ]GameObject creditUI;
 
     public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
+    }
 
+    public void CreditsMenu()
+    {
+        menuUI.SetActive(false);
+        creditUI.SetActive(true);
+
+    }
+
+    public void BackFromCredits()
+    {
+        creditUI.SetActive(false);
+        menuUI.SetActive(true);
     }
 }
